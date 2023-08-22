928952783ba0d50d1350d82e158ecff9da2d4068
xero <marius.scutea.00@gmail.com>
2023-08-22 16:45:55 +0930

    feature: Mobile sidebar format
    Sidebar covers the entire screen on mobile

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,6 +1,8 @@
 window.customElements.define('sidebar-template', class extends HTMLElement{
     connectedCallback(){
         this.innerHTML = `
+        <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+        <link rel="stylesheet" href="../styles/sidebar-style.css">
         <div class="sidebar">
             <div class="top">
                 <div class="logo">
@@ -13,11 +15,18 @@ window.customElements.define('sidebar-template', class extends HTMLElement{
             <img src="../images/me.jpg" alt="user" class="user-img" color="#ffffff">
             <!-- <img class="bx bx-user" class="user-img">-->
             <div>
-                <p class="bold">NAHLN User</p>
+                <p class="bold">NALHN User</p>
                 <p>Admin</p>
             </div>
         </div>
         <ul>
+        <li>
+            <a href="index.html">
+                <i class="bx bx-home"></i>
+                <span class="nav-item">Home</span>
+            </a>
+            <span class="tooltip">Home</span>
+            </li>
             <li>
                 <a href="#">
                     <i class="bx bxs-search"></i>
@@ -26,11 +35,11 @@ window.customElements.define('sidebar-template', class extends HTMLElement{
                 <span class="tooltip">Search</span>
             </li>
             <li>
-                <a href="#">
+                <a href="categories.html">
                     <i class="bx bx-library"></i>
-                    <span class="nav-item">Policies</span>
+                    <span class="nav-item">Categories</span>
                 </a>
-                <span class="tooltip">Policies</span>
+                <span class="tooltip">Categories</span>
             </li>
             <li>
                 <a href="#">
@@ -40,7 +49,7 @@ window.customElements.define('sidebar-template', class extends HTMLElement{
                 <span class="tooltip">Calculator</span>
             </li>
             <li>
-                <a href="#">
+                <a href="about.html">
                     <i class="bx bx-help-circle"></i>
                     <span class="nav-item">About</span>
                 </a>
@@ -58,6 +67,9 @@ document.addEventListener('DOMContentLoaded', function () {
     let mainContent = document.querySelector('.main-content')
     let open = sidebar.classList.contains("active")
 
+    if (isMobileDevice()) {
+            sidebar.classList.add("mobile");
+    }
     btn.addEventListener("click", () => {
         sidebar.classList.toggle("active");
         resizeMainContent();
@@ -77,11 +89,27 @@ document.addEventListener('DOMContentLoaded', function () {
             mainContent.style.left = '80px';
             mainContent.style.width = 'calc(100% - 80px)'
             open = false;
+            mainContent.style.visibility = "visible"
         }
         else if (!open){
             mainContent.style.left = '250px';
-            mainContent.style.width = 'calc(100% - 250px)'
+            if(isMobileDevice()) {
+                mainContent.style.width = "0"
+                mainContent.style.height = "0"
+                mainContent.style.visibility = "hidden"
+                mainContent.style.padding = "0"
+                mainContent.style.margin = "0"
+                mainContent.style.left = "100%"
+                mainContent.style.overflow = "hidden"
+            } else {
+                mainContent.style.width ='calc(100% - 250px)';
+            }
             open = true;
         }
+        
     }
+    function isMobileDevice() {
+        return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+    }
+
 });
