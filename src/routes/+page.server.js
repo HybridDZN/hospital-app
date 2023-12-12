e8b2f341483f45d42377ce4a5fda84019ea3a22b
xero <marius.scutea.00@gmail.com>
2023-12-12 21:44:22 +1030

    Adding pocketbase functionality

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,25 +1,54 @@
 import s3 from "$lib/server/s3.js";
-const bucket = "hospital-documents"
+import { pbkdf2 } from "crypto";
+import PocketBase from 'pocketbase';
+const bucket = "hospital-documents";
+const collection = "hospital_documents";
+const pb = new PocketBase('http://127.0.0.1:8090/');
 
 export async function load(){
-    const objects = await s3.listObjects({
-        Bucket: bucket,
-    });
-
-    let files = objects.Contents;
+    //list all items
+    // TODO: Pass this through in the same foramat as S3 api call. Append the URL as part of the object
+    const { items: documents } = await pb.collection(collection).getList();
     let output = {};
-
-    for (let item of files){
-        let path = item.Key.split("/");
-        let fileName = path[path.length-1];
-        let folder = path[0];
-
-        if (!output[folder]) {
-            output[folder] = [];
+    for (let document of documents){
+        const { id } = document;
+        const currentFileDetails = await pb.collection(collection).getOne(id);
+        const { file: fileName, category } = currentFileDetails;
+        const url = pb.files.getUrl(currentFileDetails, fileName);
+        const truncatedName = fileName.substring(0, fileName.lastIndexOf('_'));
+        if (!output[category]){
+            output[category] = [];
         }
-
-        output[folder].push(fileName);
+        output[category].push({
+            name: truncatedName,
+            url: url
+        });
     }
-    output = {files:output}
+    output = {files:output};
+    console.log(output);
     return output.files;
 }
+
+// S3 file getter(?)
+// export async function load(){
+//     const objects = await s3.listObjects({
+//         Bucket: bucket,
+//     });
+
+//     let files = objects.Contents;
+//     let output = {};
+
+//     for (let item of files){
+//         let path = item.Key.split("/");
+//         let fileName = path[path.length-1];
+//         let folder = path[0];
+
+//         if (!output[folder]) {
+//             output[folder] = [];
+//         }
+
+//         output[folder].push(fileName);
+//     }
+//     output = {files:output}
+//     return output.files;
+// }
