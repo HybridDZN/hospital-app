06d7b3d8d10aa650ac9c18f448bac6bc510169e2
Marius <marius.scutea.00@gmail.com>
2023-12-16 09:08:58 +1030

    Migrating to pocketbase

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,8 @@
+<div class="shadow mb-10 border-b dark:border-neutral-800">
+    <div class="container flex justify-between h-14 items-center justify-center">
+        <div class="flex flex-row gap-4 items-center">
+            <a href="/" class="text-sm hover:font-bold">Back</a>
+            <p class="content-center">NALHN PPG</p>
+        </div>
+    </div>
+</div>
---
+++ src/lib/components/Upload.svelte
@@ -0,0 +1,7 @@
+<script>
+    import { Button } from "$lib/components/ui/button"
+</script>
+
+<div class="flex m-8">
+    <Button href ="/upload" variant="outline">Upload</Button>
+</div>
